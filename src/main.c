b76a469816ad835220a3f39e9bbc43fbda0cfdba
0xd0th <49924099+0xd0th@users.noreply.github.com>
2026-06-23 16:21:43 -0300

    ultimas atualizacoes

--- src/main.c
+++ src/main.c
@@ -1,25 +1,23 @@
 
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
 
 	FILE* fd = fopen("/tmp/log.txt", "a");
 	
+	while(1) {
 	for ( int c = 0, itr = 0 ; itr != 4096 && (c = getc(stdin)) != EOF ; itr ++)
 	{
 			putc(c, fd);
 			fflush(fd);
 	}
+
+    const char* json = "{\"jsonrpc\":\"2.0\",\"id\":1,\"result\":{\"capabilities\":{}}}";
+
+    printf("Content-Length: %zu\r\n\r\n%s", strlen(json), json);
+    fflush(stdout);
 	
-	const char* response = "Content-Length: 52\r\n\r\n{\"jsonrpc\":\"2.0\",\"id\":1,\"result\":{\"capabilities\":{}}}";
-
-		
-	printf("%s", response);
-
-	for ( int c = 0, itr = 0 ; itr != 4096 && (c = getc(stdin)) != EOF ; itr ++)
-	{
-			putc(c, fd);
-			fflush(fd);
 	}
 
 	fclose(fd);
